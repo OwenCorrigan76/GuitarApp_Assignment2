6f892596d88713a129345e754095bb28f4c63ab0
owencorrigan <owencorrigan76@gmail.com>
2022-12-20 11:28:17 +0000

    Add URL links to StoreFragment

--- app/src/main/java/ie/wit/guitarApp/ui/store/StoreFragment.kt
+++ app/src/main/java/ie/wit/guitarApp/ui/store/StoreFragment.kt
@@ -6,13 +6,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import ie.wit.guitarApp.databinding.FragmentStoreBinding
 import ie.wit.guitarApp.main.MainApp
-import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SwitchCompat
-import ie.wit.guitarApp.R
 
 class StoreFragment : Fragment() {
     lateinit var app: MainApp
@@ -36,19 +32,7 @@ class StoreFragment : Fragment() {
         fragBinding.linkText.setText("guitars.com")
         fragBinding.linkText2.setText("thomann.de")
         fragBinding.linkText3.setText("musicstore.de")
-        // val switchCompat = fragBinding.switchCompat
-     /*   val switch = fragBinding.switch1
-
-        if (switch.isActivated) {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
-            switch.text = "Disable dark Mode"
-            println("disable")
-        } else {
-            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
-            switch.text = "Enable dark mode"
-            println("enable")
-        }
-*/
+        fragBinding.linkText3.setText("musicmaker.ie")
     }
 }
 
